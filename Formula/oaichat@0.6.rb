b627825b9315437b51b1cc29cadb74aecacabd3a
Alyx Ferrari <alyx@alyxandra.io>
2025-01-21 13:26:02 -0800

    Update oaichat@0.6.rb

--- Formula/oaichat@0.6.rb
+++ Formula/oaichat@0.6.rb
@@ -3,7 +3,11 @@ class oaichat06 < Formula
     homepage "https://github.com/TarballCoLtd/oaichat"
     url "https://github.com/TarballCoLtd/oaichat/archive/refs/tags/v0.6.tar.gz"
     sha256 "497b07b583d7fad536c30e0bb688db04c5b650fc3052c7fb7dc85b29d64bded2"
+    license "MIT"
+    depends_on "swift" => :build
+    depends_on macos: :ventura
     def install
-        bin.install "oaichat"
+        system "swift", "build", "--disable-sandbox", "-c", "release"
+        bin.install ".build/release/oaichat"
     end
 end
